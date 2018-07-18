6ecfa0c51dd9cafc4db25154507334d8029e2906
Peter Lewerin <peter.lewerin@tele2.se>
2018-07-18 12:15:13 +0200

    Small fix to startup script.

--- _.bat
+++ _.bat
@@ -1,1 +1,2 @@
-@gvim -p3 -c "argl topdir/lib/**/*.tcl topdir/lib/**/*.tm |argd **/pkgIndex.tcl |argu |bd **/pkgIndex.tcl" +tabn -c "argl ./**/*.test" +tabn -c "argl ./**/testreport.txt" +tabn
+rem "argl topdir/lib/**/*.tcl topdir/lib/**/*.tm |argd **/pkgIndex.tcl |argu |bd **/pkgIndex.tcl"
+@gvim -p3 -c "argl topdir/lib/**/*.tcl topdir/lib/**/*.tm" +tabn -c "argl ./**/*.test" +tabn -c "argl ./**/testreport.txt" +tabn
