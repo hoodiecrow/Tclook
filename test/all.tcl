c26255d3ff2c98f0f0a9a603d246b124d68c7a59
Peter Lewerin <peter.lewerin@tele2.se>
2018-08-07 18:35:18 +0200

    restructuring

--- test/all.tcl
+++ test/all.tcl
@@ -1,14 +1,6 @@
-package require tcltest
+set testdir [pwd]
+set project [file tail [file dirname $testdir]]
 
-set testdir [file normalize [file dirname [info script]]]
-set outfile [file join $testdir testreport.txt]
-set errfile [file join $testdir testerrors.txt]
-file delete -force $outfile $errfile
+#source -encoding utf-8 ~/code/common/starkit.tcl
+source -encoding utf-8 ~/code/common/all.tcl
 
-::tcltest::configure {*}$::argv -encoding utf-8
-
-::tcltest::configure -testdir $testdir -outfile $outfile -errfile $errfile
-::tcltest::configure -tmpdir [file join $testdir temp]
-::tcltest::configure -loadfile [file join $testdir common.tcl]
-
-::tcltest::runAllTests
--- test/common.tcl
+++ test/common.tcl
@@ -1,5 +1,2 @@
 set dir [file normalize [file join [file dirname [info script]] .. topdir lib]]
-::tcl::tm::path add $dir
-set auto_path [linsert $auto_path 0 $dir]
-
-package require log
+source -encoding utf-8 ~/code/common/common.tcl
