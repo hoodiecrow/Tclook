5063714a8361fee375d5c39d78d92d672d99b9ee
Peter Lewerin <peter.lewerin@tele2.se>
2018-08-08 20:07:58 +0200

    son of restructuring

--- test/all.tcl
+++ test/all.tcl
@@ -1,6 +1,24 @@
-set testdir [pwd]
-set project [file tail [file dirname $testdir]]
+package require tcltest
 
-#source -encoding utf-8 ~/code/common/starkit.tcl
-source -encoding utf-8 ~/code/common/all.tcl
+set ::DIRS(test) [file dirname [file normalize [info script]]]
+set ::DIRS(base) [file dirname $::DIRS(test)]
+set ::project    [string tolower [file tail $::DIRS(base)]]
+set ::DIRS(lib)  [file join $::DIRS(base) topdir lib]
 
+set outfile [file join $::DIRS(test) testreport.txt]
+set errfile [file join $::DIRS(test) testerrors.txt]
+file delete -force $outfile $errfile
+
+lappend ::argv -testdir $::DIRS(test)
+lappend ::argv -outfile $outfile
+lappend ::argv -errfile $errfile
+lappend ::argv -tmpdir [file join $::DIRS(test) temp]
+lappend ::argv -load [subst -noc {
+    ::tcl::tm::path add $::DIRS(lib)
+    set ::auto_path [linsert \$::auto_path 0 $DIRS(lib)]
+    package require $::project
+    package require log
+}]
+
+::tcltest::configure {*}$::argv
+::tcltest::runAllTests
--- test/common.tcl
+++
@@ -1,2 +0,0 @@
-set dir [file normalize [file join [file dirname [info script]] .. topdir lib]]
-source -encoding utf-8 ~/code/common/common.tcl
